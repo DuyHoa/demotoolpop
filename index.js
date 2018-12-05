c3301a972914c6ef4aa7bb025ca7e5f09238e196
DuyHoa <ndh.duyhoa.97@gmail.com>
2018-12-06 00:45:08 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+(function() {
+  var d = document,
+    h = d.getElementsByTagName('head')[0],
+    s = d.createElement('script');
+
+  s.type = 'text/javascript';
+  s.async = true;
+  s.src = 'demojs.js';
+  h.appendChild(s);
+})();
