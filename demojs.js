04ba612a2c81e4d0edb98f42fa55176eef04f153
DuyHoa <ndh.duyhoa.97@gmail.com>
2018-12-06 12:40:30 +0700

    Add files via upload

--- demojs.js
+++ demojs.js
@@ -20,84 +20,91 @@ var pos_right = '30'; //khoảng cách right
 var pos_float = 'right';//vị trí bên trái or phải(left\right)
 
 
-(function(){
-	window.onload = function(){
-		time_cal = (time_delay_icon+time_delay)*1000;
-		time_r = time_delay_icon*1000;
-		var cw1 = document.getElementById("conten_welcome1");
-		var cw2 = document.getElementById("conten_welcome2");
-		var bt1 = document.getElementById("bt1");
-		var bt2 = document.getElementById("bt2");
-		var bt3 = document.getElementById("bt3");
-		var bd = document.getElementById("bd");
+window.onload = function(){
+	time_cal = (time_delay_icon+time_delay)*1000;
+	time_r = time_delay_icon*1000;
+	var cw1 = document.getElementById("conten_welcome1");
+	var cw2 = document.getElementById("conten_welcome2");
+	var bt1 = document.getElementById("bt1");
+	var bt2 = document.getElementById("bt2");
+	var bt3 = document.getElementById("bt3");
+	var bd = document.getElementById("bd");
 		
-		cw1.innerHTML = content_welcome1;
-		cw1.style.fontFamily = "roboto";
+	cw1.innerHTML = content_welcome1;
+	cw1.style.fontFamily = "roboto";
 		
-		cw2.innerHTML = content_welcome2;
-		cw2.style.fontFamily = "roboto";
+	cw2.innerHTML = content_welcome2;
+	cw2.style.fontFamily = "roboto";
 
-		bd.style.bottom = pos_bottom;
-		bd.style.right = pos_right;
-		bd.style.float = pos_float;
+	bd.style.bottom = pos_bottom;
+	bd.style.right = pos_right;
+	bd.style.float = pos_float;
 		
 		
-		//cai dat link button
-		bt1.innerHTML = button_func_1;
-		bt1.setAttribute("href",url_link_1);
-		bt1.style.fontFamily = font_;
-		bt2.innerHTML = button_func_2;
-		bt2.setAttribute("href",url_link_2);
-		bt2.style.fontFamily = font_;
-		bt3.innerHTML = button_func_3;
-		bt3.setAttribute("href",url_link_3);
-		bt3.style.fontFamily = font_;
+	//cai dat link button
+	bt1.innerHTML = button_func_1;
+	bt1.setAttribute("href",url_link_1);
+	bt1.style.fontFamily = font_;
+	bt2.innerHTML = button_func_2;
+	bt2.setAttribute("href",url_link_2);
+	bt2.style.fontFamily = font_;
+	bt3.innerHTML = button_func_3;
+	bt3.setAttribute("href",url_link_3);
+	bt3.style.fontFamily = font_;
+
+	
+	setTimeout(function(){
+		document.getElementById("bd").style.display = 'block';
+	},time_r);
+	//if()
+	//time tre hien message
+	//if(document.getElementById("cs").style.display == 'none'){
+	setTimeout(function(){
+			document.getElementById("news").style.display = 'block';
 		
+	},time_cal);
 		
-		setTimeout(function(){
-			document.getElementById("bd").style.display = 'block';
-		},time_r);
-		//if()
-		//time tre hien message
-		//if(document.getElementById("cs").style.display == 'none'){
-		setTimeout(function(){
-				document.getElementById("news").style.display = 'block';
-			
-		},time_cal);
-			
-		setTimeout(function(){
-			
-				document.getElementById("m").style.display = 'block';
-			
-		},time_cal+1000);
-		//}
-		document.getElementById("cl").onclick = function(){
-			if(document.getElementById("cs").style.display == 'block')
-				turn_off();
-		};
-		document.getElementById("mb").onclick = function () {
-			if(document.getElementById("cs").style.display == 'none'){
-				turn_on();
-			}
-			else{
-				turn_off();
-			}	
-		};
+	setTimeout(function(){
+		
+			document.getElementById("m").style.display = 'block';
+		
+	},time_cal+1000);
+	//}
+	document.getElementById("cl").onclick = function(){
+		if(document.getElementById("cs").style.display == 'block')
+			turn_off();
 	};
+	document.getElementById("mb").onclick = function () {
+		if(document.getElementById("cs").style.display == 'none'){
+			turn_on();
+		}
+		else{
+			turn_off();
+		}	
+	};
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+};
 
-	function turn_on(){
-		document.getElementById("cs").style.display = 'block';
-		document.getElementById("m").style.display = 'none';
-		document.getElementById("news").style.display = 'none';
-		document.getElementById("im").setAttribute("href", url_img_close);
-	}
-
-	function turn_off(){
-		document.getElementById("cs").style.display = 'none';
-		document.getElementById("im").setAttribute("href", url_img_icon);
-	}
-
-}).call(this);
+function turn_on(){
+	document.getElementById("cs").style.display = 'block';
+	document.getElementById("m").style.display = 'none';
+	document.getElementById("news").style.display = 'none';
+	document.getElementById("im").setAttribute("href", url_img_close);
+}
+function turn_off(){
+	document.getElementById("cs").style.display = 'none';
+	document.getElementById("im").setAttribute("href", url_img_icon);
+}
 
 
 
--- index.js
+++ index.js
@@ -1,10 +1,41 @@
-(function() {
-  var d = document,
-    h = d.getElementsByTagName('head')[0],
-    s = d.createElement('script');
-
-  s.type = 'text/javascript';
-  s.async = true;
-  s.src = 'demojs.js';
-  h.appendChild(s);
+(function(){
+	window.onload=function(){
+		var a = document.getElementsByTagName("head");
+		var b = document.createElement("script");
+		var div = document.createElement('div');
+		var d = document.createElement("link");
+		var c = document.createElement("link");
+		var e = document.createElement("meta");
+		
+		e.charset="utf-8";
+		a[0].appendChild(e);
+		
+		b.language = "JavaScript";
+		b.type = "text/javascript";
+		b.src = "demojs.js";
+		
+		a[0].appendChild(b);
+		
+		
+		c.type = "text/css";
+		c.rel = "stylesheet";
+		c.src = "demostyle.css";
+		
+		a[0].appendChild(c);
+		
+		
+		c.rel = "stylesheet";
+		c.href = "https://fonts.googleapis.com/css?family=Roboto&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese";
+		
+		a[0].appendChild(d);
+		
+		div.className = "sketon_div";
+		document.body.appendChild(div);
+		
+		$("div").load("demoProject.html");
+		
+		
+	}
+	
+	
 })();
